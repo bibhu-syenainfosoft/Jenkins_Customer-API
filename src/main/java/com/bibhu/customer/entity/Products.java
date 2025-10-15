7f0468cf6cbda401313dd0319b9e6eb6acb73835
Bibhudatta Behera <143987793+bibhu-syenainfosoft@users.noreply.github.com>
2025-10-15 10:01:25 +0530

    done

--- src/main/java/com/bibhu/customer/entity/Products.java
+++ src/main/java/com/bibhu/customer/entity/Products.java
@@ -8,34 +8,6 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.math.BigDecimal;
 import java.time.Instant;
 
-//@Entity
-//@Data
-//@EntityListeners(AuditingEntityListener.class)
-//public class Products {
-//    @Id
-//    @GeneratedValue(strategy=GenerationType.SEQUENCE)
-//    private long productId;
-//    private String productName;
-//    private String productDescription;
-//    private BigDecimal productPrice;
-//    private String productImageUrl;
-//    private String productTitle;
-//    private String status;
-//    private long noOfStocks;
-//    @CreatedDate
-//    @Column(nullable = false, updatable = false)
-//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Hyderabad")
-//    private Instant createdDate = Instant.now();
-//
-//    @LastModifiedDate
-//    private Instant updatedDate;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "category_Id",nullable = false)
-//    private Categories categories;
-//}
-
-
 @Entity
 @Data
 public class Products {
